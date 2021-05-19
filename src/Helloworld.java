5dc30b2445c1da10a7fe25be7dc874a970556056
hkabi <hkabi@221.149.140.184>
2021-05-19 22:51:18 +0900

    첫번째 commit

---
+++ src/Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld {
+   public static void main(String[] args) {
+	   System.out.println("Hello World\n");
+	   System.out.println("Hey World \n");
+   }  // 실행 : ctrl + F11 / 여러 소스 생성 : ctrl + n
+}
+
