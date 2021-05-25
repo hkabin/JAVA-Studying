83c792fbe8315d518f10c68830aab0448196415b
hkabi <hkabi@125.143.128.196>
2021-05-25 22:48:28 +0900

    구구단 2단,3단 첫 시도

---
+++ src/GuGudan2.java
@@ -0,0 +1,15 @@
+public class GuGudan2 {
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 1;
+		
+		for(int i = 1; i < 10; i++)
+		{
+			int gob2 = a*i;
+			System.out.println(gob2);
+		}
+	}
+}
+
+
---
+++ src/GuGudan3.java
@@ -0,0 +1,15 @@
+public class GuGudan3 {
+	public static void main(String[] args) {
+		
+		int x = 3;
+		
+		for(int y = 1; y <= 9; y++)
+		{
+			int gob3 = x*y;
+			System.out.println(gob3);
+		}
+			
+		
+	}
+	
+}
